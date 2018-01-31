810dc5c4920df1ad192995cbeac5a29623e54947
Maninder1995 <Maninder1995@github.com>
2018-01-31 13:55:47 +0000

    task1

---
+++ task1.py
@@ -0,0 +1,14 @@
+class my_ipadd():
+   def__init__(self,oct_a,oct_b,oct_c,oct,d):
+       self.a=oct_a
+       self,b=oct_b
+       self.c=oct_c
+       self.d=oct_d
+       self.netmask=subnet
+  def__str__():
+  	return(str(Self.a)+"."+...)
+  def get_broadcast(self):
+    result=my_ipadd(self.a&self.b&subnet.b...)
+    
+Myadd_host1=my_ipadd(10,2,3,4,24)
+
